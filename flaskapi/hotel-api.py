e5b2650ec2b679a4be059b93601f11e4c39e0cb4
Azudeus <blitzthunder68@yahoo.com>
2017-11-14 03:58:57 +0700

    Add create book, list book, list room, check availability

--- flaskapi/hotel-api.py
+++ flaskapi/hotel-api.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL 
+import MySQLdb
 
 # pip install virtualenv
 # venv/bin/activate
@@ -13,25 +14,46 @@ app.config['MYSQL_DB'] = 'hotel_booking'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def init():
-	cur = mysql.connection.cursor()
+# Book
+@app.route('/book/list', methods = ['GET'])
+def list_book():
+	cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 	cur.execute('''SELECT * FROM book''')
 	rv = cur.fetchall()
-	return str(rv)
-
-@app.route('/hello/<name>')
-def hello_world(name):
-	return "Hello %s!" % name
+	return jsonify(rv)
 
 @app.route('/book/create', methods = ['POST'])
 def create_book():
 	if request.method == "POST":
-		user = request.form['name']
-		return "%s" % user
-	else:
-		return 0
+		customer_id = request.form['customer_id']
+		paid_price = request.form['paid_price']
+		room_id = request.form['type']
+		amount = request.form['amount']
+		cur = mysql.connection.cursor()
+		queryCreate = '''INSERT INTO book (customer_id, paid_price, type, amount) VALUES (%s, %s, '%s', %s)''' % (customer_id, paid_price, room_id, amount)
+		cur.execute(queryCreate)
+		queryUpdate = '''UPDATE room SET stock = stock - %s''' % (amount)
+		cur.execute(queryUpdate)
+		mysql.connection.commit()
+		return ""
 
+# Room
+@app.route('/room/list', methods = ['GET'])
+def list_room():
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT * FROM room''')
+	rv = cur.fetchall()
+	return jsonify(rv)
+
+@app.route('/room/check/<room_id>')
+def check_availability(room_id):
+	cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+	cur.execute('''SELECT stock FROM room WHERE (type = '%s')''' % room_id ) 
+	rv = cur.fetchall()
+	if rv[0]['stock'] > 0:
+		return "Available"
+	else:
+		return "Not Available"
 if __name__ == '__main__':
 	app.run()
 

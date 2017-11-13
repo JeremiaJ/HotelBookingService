6822e6969a2ccbe1e0afcc78b704c926bdceaa5c
JeremiaJ15 <jeremia_jason@yahoo.com>
2017-11-13 15:49:12 +0700

    First Commit

---
+++ hotel-booking/src/main/java/com/camunda/bpm/ppls/hotelbooking/HotelBookingApplication.java
@@ -0,0 +1,9 @@
+package com.camunda.bpm.ppls.hotelbooking;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class HotelBookingApplication extends ServletProcessApplication {
+  // empty implementation
+}

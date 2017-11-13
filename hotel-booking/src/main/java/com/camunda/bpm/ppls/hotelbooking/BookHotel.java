f37e90d649dbdcd02140387b8ba772edd1b9d8d6
JeremiaJ15 <jeremia_jason@yahoo.com>
2017-11-14 00:13:06 +0700

    tambah satu service task dari tutorial

---
+++ hotel-booking/src/main/java/com/camunda/bpm/ppls/hotelbooking/BookHotel.java
@@ -0,0 +1,15 @@
+package com.camunda.bpm.ppls.hotelbooking;
+
+import java.util.logging.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class BookHotel implements JavaDelegate {
+
+  private final static Logger LOGGER = Logger.getLogger("LOAN-REQUESTS");
+
+  public void execute(DelegateExecution execution) throws Exception {
+    LOGGER.info("Processing request by '" + execution.getVariable("customerId") + "'...");
+  }
+
+}
